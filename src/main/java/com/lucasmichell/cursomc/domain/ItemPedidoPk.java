344d6178ce3fe700c46ffc899edd67ae99d28147
lucasmichel <lucasmichel@hotmail.com>
2020-08-08 18:03:55 -0300

    Atualizacao jsonignore

--- src/main/java/com/lucasmichell/cursomc/domain/ItemPedidoPk.java
+++ src/main/java/com/lucasmichell/cursomc/domain/ItemPedidoPk.java
@@ -2,7 +2,6 @@ package com.lucasmichell.cursomc.domain;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
--- src/main/java/com/lucasmichell/cursomc/domain/Pedido.java
+++ src/main/java/com/lucasmichell/cursomc/domain/Pedido.java
@@ -27,13 +27,11 @@ public class Pedido implements Serializable {
 	private Integer id;
 	
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-	private Date instante;	
+	private Date instante;		
 	
-	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL , mappedBy = "pedido")	
-	private Pagamento pagamento;
+	private Pagamento pagamento;	
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;	
