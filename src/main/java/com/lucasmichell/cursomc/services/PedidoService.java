45ca7b286ece7360823db071e88dcdb36bcd69c8
lucasmichel <lucasmichel@hotmail.com>
2020-08-08 20:03:01 -0300

    Inserindo uma nova categoria com o POST

--- src/main/java/com/lucasmichell/cursomc/services/PedidoService.java
+++ src/main/java/com/lucasmichell/cursomc/services/PedidoService.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lucasmichell.cursomc.domain.Categoria;
 import com.lucasmichell.cursomc.domain.Pedido;
-import com.lucasmichell.cursomc.repositories.CategoriaRepository;
 import com.lucasmichell.cursomc.repositories.PedidoRepository;
 import com.lucasmichell.cursomc.services.exceptions.ObjectNotFoundException;
 
