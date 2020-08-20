63a67704bf5b78e19228b39125b1bbffbf64c20a
lucasmichel <lucasmichel@hotmail.com>
2020-08-20 19:38:57 -0300

    endpoint Categorias

--- src/main/java/com/lucasmichell/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucasmichell/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.lucasmichell.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasmichell.cursomc.domain.Categoria;
+import com.lucasmichell.cursomc.dto.CategoriaDTO;
 import com.lucasmichell.cursomc.services.CategoriaService;
 
 @RestController
@@ -49,6 +52,14 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	
+	}
+	
+	@RequestMapping( method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); 
+		return ResponseEntity.ok(listDto);
 	
 	}
 	
